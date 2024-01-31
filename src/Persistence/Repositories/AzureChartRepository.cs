ed80cdebe6da2e78fc8e2e68021ccd4665eef91b
Pete Russell <pete.russell01@gmail.com>
2024-01-31 17:09:57 -0600

    Ported Fetcher from flightwx.info. Copied/updated Dockerfile from API project.

---
+++ src/Persistence/Repositories/AzureChartRepository.cs
@@ -0,0 +1,9 @@
+namespace Persistence.Repositories;
+
+class AzureChartRepository : IChartRepository
+{
+    public bool Exists(string filename)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Persistence/Repositories/IChartRepository.cs
@@ -0,0 +1,6 @@
+namespace Persistence.Repositories;
+
+interface IChartRepository
+{
+    public bool Exists(string filename);
+}
